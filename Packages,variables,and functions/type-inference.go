2a9f32f9e7a1474aab01edf16bf5f9a9392d8202
avinashkunuje <itsavi.ks@gmail.com>
2015-09-01 01:29:33 +0530

    Type inference

---
+++ Packages,variables,and functions/type-inference.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me!
+	i := 42           // int
+	f := 3.142        // float64
+	g := 0.867 + 0.5i // complex128
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("v is of type %T\n", i)
+	fmt.Printf("v is of type %T\n", f)
+	fmt.Printf("v is of type %T\n", g)
+}
